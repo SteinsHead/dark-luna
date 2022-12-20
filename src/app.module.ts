8978817257b2fb1225090d9626deef62623bec64
christina <headsteins@gmail.com>
2022-12-20 22:29:41 +0800

    add mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DiaryController } from './diary/diary.controller';
@@ -8,7 +9,7 @@ import { BooksController } from './diary/books/books.controller';
 import { AnimationController } from './animation/animation.controller';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/luna')],
   controllers: [AppController, DiaryController, ThinkController, AboutController, BooksController, AnimationController],
   providers: [AppService],
 })
